a9027042078e2b6b1e32ba1447cf5a66205043f4
Preetesh21 <verma.preetesh21@gmail.com>
2021-04-03 23:47:13 +0530

    minor changes

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ require('dotenv').config()
 const Pool=require("pg").Pool;
 
 const devConfig={
-    user:process.env.user,
-    password: process.env.password,
-    host:process.env.host,
-    port:5432,
-    database: process.env.database,
+    user:process.env.pg_user,
+    password: process.env.pg_password,
+    host:process.env.pg_host,
+    port:process.env.PG_PORT,
+    database: process.env.pg_database,
 }
 
 const proConfig = process.env.DATABASE_URL; //heroku addons
